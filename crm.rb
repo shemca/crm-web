24a2aae48431f5d97267cdcf4ca747715e0de87f
Shem <shemca@gmail.com>
2015-04-13 16:36:15 -0400

    Totally Finished

--- crm.rb
+++ crm.rb
@@ -1,12 +1,36 @@
 require 'sinatra'
-require './contact'
-require './rolodex'
-	
-@@rolodex = Rolodex.new
-#	$rolodex = Rolodex.new
-# contact = @@rolodex.find(1000)
-@@rolodex.add_contact(Contact.new("Johnny", "Bravo", "johnny@bitmakerlabs.com", "Rockstar"))
-@@rolodex.add_contact(Contact.new("Realy", "McNamerson", "real@name.com", "Totally a real person"))
+# require './rolodex'
+require 'data_mapper'
+
+DataMapper.setup(:default,"sqlite3:database.sqlite3")
+
+class Contact
+	include DataMapper::Resource
+
+	property :id, Serial
+	property	:first_name, String
+	property	:last_name, String
+	property :email, String
+	property :note, String
+
+	# attr_accessor :id, :first_name, :last_name, :email, :note
+
+	# def initialize(first_name, last_name, email, note)
+	# 	@first_name = first_name
+	# 	@last_name = last_name
+	# 	@email = email
+	# 	@note = note
+	# end
+end
+
+DataMapper.finalize
+DataMapper.auto_upgrade!
+
+# @@rolodex = Rolodex.new
+# #	$rolodex = Rolodex.new
+# # contact = @@rolodex.find(1000)
+# @@rolodex.add_contact(Contact.new("Johnny", "Bravo", "johnny@bitmakerlabs.com", "Rockstar"))
+# @@rolodex.add_contact(Contact.new("Realy", "McNamerson", "real@name.com", "Totally a real person"))
 
 get '/' do
 	@crm_app_name="My CRM"
@@ -18,31 +42,40 @@ get "/contacts" do
 end
 
 get '/display' do
-	@contacts = @@rolodex.contacts
-	if query = params[:search]
-		query = /#{query}/i
-		@contacts = @contacts.select { |contact| contact.first_name =~ query }
-	end
-
-	erb :display
+	@contacts = Contact.all
+	erb:display
 end
+
+# get '/display' do
+# 	@contacts = @@rolodex.contacts
+# 	if query = params[:search]
+# 		query = /#{query}/i
+# 		@contacts = @contacts.select { |contact| contact.first_name =~ query }
+# 	end
+
+# 	erb :display
+# end
 
 
 post '/contacts' do
-	new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
-	@@rolodex.add_contact(new_contact)
-	redirect to ('/contacts')
+	new_contact = Contact.create(
+		first_name: params[:first_name], 
+		last_name: params[:last_name], 
+		email: params[:email], 
+		note: params[:note]
+		)
+	redirect to ('/display')
 end
 
 get "/contacts/:id" do
-	@contact = @@rolodex.find(params[:id].to_i)
+	@contact = Contact.get(params[:id].to_i)
 		erb :show_contact
 		
 	end
 
 
 get "/contacts/:id/edit" do
-	@contact = @@rolodex.find(params[:id].to_i)
+	@contact = Contact.get(params[:id])
 	if @contact
 		erb :edit_contact
 	else
@@ -51,7 +84,7 @@ get "/contacts/:id/edit" do
 end
 
 put "/contacts/:id" do
-	@contact = @@rolodex.find(params[:id.to_i])
+	@contact = Contact.get(params[:id])
 
 	if @contact
 		@contact.first_name = params[:first_name]
@@ -59,18 +92,20 @@ put "/contacts/:id" do
 		@contact.email = params[:email]
 		@contact.note = params[:note]
 	
-		
+	@contact.save
+
+		redirect to("/display")
 	else
 		raise Sinatra::NotFound
 	end
 end
 
 delete "/contacts/:id" do
-	@contact = @@rolodex.find(params[:id].to_i)
+	@contact = Contact.get(params[:id])
 
 	if @contact
-		@@rolodex.remove_contact(@contact)
-		redirect to("/contacts")
+		@contact.destroy
+		redirect to("/display")
 	else
 		raise Sinatra::NotFound
 	end
